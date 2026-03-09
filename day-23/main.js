845342f2eb17339db3a76b181361b00ea1921ec9
AsadMuxtorov <asadbekmuxtorov0@gmail.com>
2026-03-09 18:09:45 +0500

    filter

---
+++ day-23/main.js
@@ -0,0 +1,301 @@
+const todoValue = document.getElementById('todo-value')
+const todoAdd = document.getElementById('todo-add')
+const todoList = document.getElementById('todo-list')
+const advice = document.getElementById('advice-text')
+const newBtn = document.getElementById('new-advice')
+const allBtn = document.getElementById('all')
+const completedBtn = document.getElementById('completed')
+const pendingBtn = document.getElementById('pending')
+const searchInput = document.getElementById('search-panel')
+const themeToggleBtn = document.getElementById('theme-toggle')
+const darkIcon = document.getElementById('theme-toggle-dark-icon')
+const lightIcon = document.getElementById('theme-toggle-light-icon')
+const sortSelect = document.getElementById('sort-tasks')
+const addSound = new Audio('sounds/freesound_crunchpixstudio-add-408457.mp3')
+const deleteSound = new Audio(
+	'sounds/spinopel-remove-charging-cable-into-smartphone-393112.mp3'
+)
+addSound.load()
+deleteSound.load()
+addSound.volume = 0.2
+deleteSound.volume = 0.2
+//  (STATE)
+let currentFilter = 'all'
+let tasks = JSON.parse(localStorage.getItem('tasks')) || []
+let draggedItemIndex = null
+//  THEME
+function initTheme() {
+	const isDark =
+		localStorage.getItem('color-theme') === 'dark' ||
+		(!('color-theme' in localStorage) &&
+			window.matchMedia('(prefers-color-scheme: dark)').matches)
+
+	if (isDark) {
+		document.documentElement.classList.add('dark')
+		lightIcon.classList.remove('hidden')
+	} else {
+		document.documentElement.classList.remove('dark')
+		darkIcon.classList.remove('hidden')
+	}
+}
+themeToggleBtn.addEventListener('click', () => {
+	darkIcon.classList.toggle('hidden')
+	lightIcon.classList.toggle('hidden')
+	const isDark = document.documentElement.classList.toggle('dark')
+	localStorage.setItem('color-theme', isDark ? 'dark' : 'light')
+})
+//  (HELPERS)
+function updateStats() {
+	const allCount = tasks.length
+	const completedCount = tasks.filter(t => t.completed).length
+	const pendingCount = allCount - completedCount
+
+	allBtn.innerText = `Hammasi ${allCount}`
+	completedBtn.innerText = `Bajarildi ${completedCount}`
+	pendingBtn.innerHTML = `Kutilmoqda ${pendingCount}`
+}
+
+// Bu funksiya ilovangning "Yuragi". Nima o'zgarsa ham, shuni chaqiramiz.
+function syncTasks() {
+	localStorage.setItem('tasks', JSON.stringify(tasks)) // 1. Saqlaydi
+	updateStats() // 2. Hisoblaydi
+	renderTasks() // 3. Chizadi
+}
+
+function showToast(message, type = 'success') {
+	const container = document.getElementById('toast-container')
+	const toast = document.createElement('div')
+	let bgColor = 'bg-green-500'
+	if (type === 'error') bgColor = 'bg-red-500'
+	if (type === 'info') bgColor = 'bg-blue-500'
+
+	toast.className = `${bgColor} text-white px-6 py-3 max-sm:py-1 max-sm:px-4 rounded-lg shadow-lg transform transition-all duration-300 translate-y-10 opacity-0 flex items-center gap-2 font-medium`
+	toast.innerHTML = `<span>${message}</span>`
+	container.appendChild(toast)
+	setTimeout(() => {
+		toast.classList.remove('translate-y-10', 'opacity-0')
+	}, 100)
+	setTimeout(() => {
+		toast.classList.add('opacity-0', 'scale-90')
+		setTimeout(() => toast.remove(), 300)
+	}, 3000)
+}
+// Create element
+function createTaskElement(task, highlightedText) {
+	const realIndex = tasks.indexOf(task)
+	const newLi = document.createElement('li')
+	newLi.draggable = true
+	newLi.className = `task-animate flex items-center justify-between p-4 mb-2 rounded-xl border transition-all cursor-pointer ${
+		task.completed
+			? 'bg-green-50 border-green-200 dark:bg-green-900/20 dark:border-green-800'
+			: 'bg-white border-blue-100 shadow-sm hover:shadow-md dark:bg-slate-800 dark:border-slate-700'
+	}`
+	newLi.innerHTML = `
+		    <div class="flex flex-col flex-1 min-w-0 ">
+								<span class="text-lg break-words overflow-hidden leading-tight ${
+									task.completed
+										? 'line-through text-gray-400 dark:text-gray-500'
+										: 'text-gray-700 font-medium dark:text-gray-200'
+								}">${highlightedText || task.text}</span> 
+								<div class="flex items-center gap-3 mt-1">
+										<span class="text-[11px] text-gray-400 font-medium">${
+											task.date || 'Hozir'
+										}</span>
+								</div>
+					</div>
+        <div class="flex flex-col gap-1">
+            <button class="edit-btn ml-4 active:scale-95 transition-transform text-[11px] max-w-[90px] font-bold uppercase bg-blue-100 text-blue-500 px-3 py-1.5 rounded-lg hover:bg-blue-500 hover:text-white transition-all">Tahrirlash</button>
+            <button class="delete-btn ml-4 text-[11px] max-w-[90px] w-full font-bold uppercase bg-red-100 text-red-500 px-3 py-1.5 rounded-lg hover:bg-red-500 active:scale-95 transition-transform hover:text-white transition-all">O'chirish</button>
+        </div>
+    `
+
+	newLi.ondragstart = () => {
+		draggedItemIndex = tasks.indexOf(task)
+		newLi.classList.add('scale-105 color-red-100')
+	}
+	newLi.ondragover = e => {
+		e.preventDefault()
+		newLi.classList.add('border-t-4', 'border-blue-500')
+	}
+
+	newLi.ondragleave = () => {
+		newLi.classList.remove('border-t-4', 'border-blue-500')
+	}
+	newLi.ondrop = () => {
+		newLi.classList.remove('border-t-4', 'border-blue-500')
+		const droppedItemIndex = tasks.indexOf(task)
+		swapTasks(draggedItemIndex, droppedItemIndex)
+	}
+	// Click
+	newLi.onclick = e => {
+		const clickedText = e.target.innerText.toUpperCase()
+		if (clickedText === "O'CHIRISH") {
+			newLi.classList.add('task-exit')
+			deleteSound.play()
+			showToast("Vazifa o'chirildi!", 'error')
+			setTimeout(() => {
+				newLi.remove()
+				const index = tasks.indexOf(task)
+				if (index > -1) {
+					tasks.splice(index, 1)
+					syncTasks()
+				}
+			}, 400)
+		} else if (clickedText === 'TAHRIRLASH') {
+			editTask(tasks.indexOf(task))
+		} else {
+			toggleTask(tasks.indexOf(task))
+		}
+	}
+	return newLi
+}
+//  RENDER
+function renderTasks() {
+	todoList.innerHTML = ''
+	updateStats()
+	const searchText = searchInput.value.trim().toLowerCase()
+	let filtered = tasks.filter(task => {
+		const matchesFilter =
+			currentFilter === 'all' ||
+			(currentFilter === 'completed' ? task.completed : !task.completed)
+		const matchesSearch = task.text.toLowerCase().includes(searchText)
+		return matchesFilter && matchesSearch
+	})
+	const sortValue = document.getElementById('sort-tasks').value
+	if (sortValue === 'alphabet') {
+		filtered.sort((a, b) => a.text.localeCompare(b.text))
+	} else if (sortValue === 'completed-first') {
+		filtered.sort((a, b) =>
+			a.completed === b.completed ? 0 : a.completed ? -1 : 1
+		)
+	} else {
+		filtered.reverse()
+	}
+
+	filtered.forEach(task => {
+		let displayText = task.text
+		if (searchText !== '') {
+			const regex = new RegExp(`(${searchText})`, 'gi')
+			displayText = task.text.replace(
+				regex,
+				'<mark class="bg-yellow-300">$1</mark>'
+			)
+		}
+		todoList.appendChild(createTaskElement(task, displayText))
+	})
+}
+// (ACTIONS)
+function deleteTask(index) {
+	tasks.splice(index, 1)
+	syncTasks()
+}
+
+function toggleTask(index) {
+	tasks[index].completed = !tasks[index].completed
+	syncTasks()
+}
+
+function editTask(index) {
+	const newText = prompt('Vazifani tahrirlash:', tasks[index].text)
+	if (newText && newText.trim()) {
+		tasks[index].text = newText.trim()
+		syncTasks()
+	}
+	showToast("O'zgarish saqlandi!", 'info')
+}
+
+function setFilter(f) {
+	currentFilter = f
+	renderTasks()
+}
+
+function swapTasks(fromIndex, toIndex) {
+	if (fromIndex === toIndex) return
+	const movedTask = tasks.splice(fromIndex, 1)[0]
+	tasks.splice(toIndex, 0, movedTask)
+	syncTasks()
+}
+//  API VA HODISALAR
+function getAdvice() {
+	const adviceText = document.getElementById('advice-text')
+	advice.innerText = 'yuklanmoqda...'
+	fetch('https://api.adviceslip.com/advice')
+		.then(res => res.json())
+		.then(data => {
+			adviceText.innerText = data.slip.advice
+			localStorage.setItem('lastAdvice', data.slip.advice)
+		})
+	showToast('Yangi maslahat! ', 'info')
+}
+
+window.addEventListener(
+	'click',
+	() => {
+		addSound.load()
+		deleteSound.load()
+	},
+	{ once: true }
+)
+todoAdd.addEventListener('click', async () => {
+	if (!todoValue.value.trim()) return
+	const now = new Date()
+	const dateString = `${now.getDate().toString().padStart(2, '0')}.${(
+		now.getMonth() + 1
+	)
+		.toString()
+		.padStart(2, '0')} ${now.getHours()}:${now
+		.getMinutes()
+		.toString()
+		.padStart(2, '0')}`
+
+	const newTask = { text: todoValue.value, completed: false, date: dateString }
+	tasks.push(newTask)
+	try {
+		addSound.currentTime = 0
+		await addSound.play()
+	} catch (err) {
+		console.log('Ovoz chalishda xato:', err)
+	}
+	const newLi = createTaskElement(newTask)
+	todoList.prepend(newLi)
+
+	syncTasks()
+	showToast("Vazifa qo'shildi!")
+	todoValue.value = ''
+})
+// INIT
+searchInput.addEventListener('input', renderTasks)
+newBtn.addEventListener('click', getAdvice)
+
+initTheme()
+renderTasks()
+
+const savedAdvice = localStorage.getItem('lastAdvice')
+
+if (!savedAdvice) {
+	getAdvice()
+} else {
+	const adviceTextSpan = document.getElementById('advice-text')
+	if (adviceTextSpan) {
+		adviceTextSpan.innerText = savedAdvice
+	}
+}
+
+const clearBtn = document.getElementById('clear-completed')
+
+clearBtn.onclick = () => {
+	const hasCompleted = tasks.some(t => t.completed)
+
+	if (hasCompleted) {
+		if (confirm("Barcha bajarilgan vazifalarni o'chirmoqchimisiz?")) {
+			tasks = tasks.filter(t => !t.completed)
+			syncTasks()
+			showToast('Bajarilgan vazifalar tozalandi!', 'info')
+		}
+	} else {
+		showToast("Hali bajarilgan vazifalar yo'q!", 'info')
+	}
+}
+
+// Saralash turi o'zgarganda darhol renderTasks-ni chaqiramiz
+sortSelect.addEventListener('change', renderTasks)
